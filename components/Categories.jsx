07847151b66619ca453b2bd8de1f3b457035f614
BBakula <bozo.bakula@gmail.com>
2023-02-17 15:50:30 +0100

    update

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -5,15 +5,15 @@ const Categories = ({ categories }) => {
   return (
     <div className="flex justify-between items-center w-full px-10 py-4">
       {categories.map((category) => (
-      <div key={category._id} className="flex flex-col justify-center items-center min-w-fit">
+      <div key={category._id} className="flex flex-col justify-center px-3 py-2 items-center min-w-fit">
         <div style={{ backgroundColor: category.bgcolor }} className={`flex justify-center items-center border w-16 h-16 rounded-full `}>
-          <Image 
+          {/* <Image 
             src={category.imageUrl}
             alt={category.description}
             width={50}
             height={50}   
             className="object-contain hover:scale-110 transition-all cursor-pointer"       
-          />
+          /> */}
         </div>
         <h3 className="text-sm mt-1 w-full text-center">{category.description}</h3>
       </div>  
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqDflmmZ-GdGLw6VG2nhWsqk5GDRh8WpE",
+  authDomain: "olx-clone-3ec0a.firebaseapp.com",
+  projectId: "olx-clone-3ec0a",
+  storageBucket: "olx-clone-3ec0a.appspot.com",
+  messagingSenderId: "989346871515",
+  appId: "1:989346871515:web:e14d2a1d1e601794fd34fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
