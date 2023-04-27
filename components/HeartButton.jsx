809f9df1993e6de129f4185a0507d70a4cd98c0f
BBakula <bozo.bakula@gmail.com>
2023-04-27 16:09:56 +0200

    update

--- components/HeartButton.jsx
+++ components/HeartButton.jsx
@@ -2,33 +2,76 @@
 
 import { HeartIcon } from "@heroicons/react/24/solid";
 import { HeartIcon as Outline } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDocumentData } from "react-firebase-hooks/firestore";
-import { doc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
+import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { selectUserID } from "@/redux/slice/authSlice";
 
 const HeartButton = ({ like, id }) => {
   const [clicked, setClicked] = useState(false)
-  const [likedAdds] = useDocumentData(doc(db, "products", id))
+  const [product, setProduct] = useState(null);
+  const [likedAddsRef, setLikedAddsRef] = useState(null);
+  const userId = useSelector(selectUserID)
 
-  console.log(likedAdds)
+  useEffect(() => {
+    const docRef = doc(db, "products", id);
+    getDoc(docRef).then((docSnap) => {
+      if (docSnap.exists()) {
+        setProduct(docSnap.data());
+      }
+    });
+  }, [id]);
   
-  const handleLike = () => {
-    setClicked(prev => !prev)
-
-    try {
-      
-    } catch (error) {
-      
+  const toggleLike = async () => {
+    // Check if the user is logged in
+    if (!userId) {
+      toast.error("Please login to like products.");
+      return;
     }
 
-  }
+    // Check if the product is already liked
+    if (product && product.liked) {
+      // Remove the product from likedAdds collection in Firestore
+      if (likedAddsRef) {
+        await deleteDoc(likedAddsRef);
+      }
+      // Update the product document in Firestore
+      await updateDoc(doc(db, "products", id), { liked: false });
+      // Update the product state
+      setProduct({ ...product, liked: false });
+      // Update the clicked state
+      setClicked(false);
+      toast.success("Product removed from liked list.");
+    } else {
+      // Add the product to likedAdds collection in Firestore
+      if (!likedAddsRef) {
+        const likedAddsCollectionRef = collection(
+          db,
+          "users",
+          userId,
+          "savedAdds"
+        );
+        const newLikedAddRef = await addDoc(likedAddsCollectionRef, { productId: id });
+        setLikedAddsRef(newLikedAddRef);
+      }
+      // Update the product document in Firestore
+      await updateDoc(doc(db, "products", id), { liked: true });
+      // Update the product state
+      setProduct({ ...product, liked: true });
+      // Update the clicked state
+      setClicked(true);
+      toast.success("Product added to liked list.");
+    }
+  };
 
   return (
-    <div onClick={() => setClicked(!clicked)} className="flex justify-center items-center">
-      {clicked ?
-        <Outline className="w-8 h-8 mx-1 cursor-pointer transition hover:scale-110" /> :
-        <HeartIcon className="w-8 h-8 mx-1 cursor-pointer transition hover:scale-110 text-red-600" />
+    <div onClick={toggleLike} className="flex justify-center items-center">
+      {clicked || (product && product.liked) ?
+        <HeartIcon className="w-8 h-8 mx-1 cursor-pointer transition hover:scale-110 text-red-600" /> :
+        <Outline className="w-8 h-8 mx-1 cursor-pointer transition hover:scale-110" />
       }
     </div>
   );
